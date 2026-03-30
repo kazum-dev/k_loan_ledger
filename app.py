c8cc7963695cec935f05a24faeb5012ea655c7a0
k <pass805549@gmail.com>
2026-03-30 11:58:38 +0900

    E-3 CSV件数表示機能を実装（FlaskでCSV→HTML表示）

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
+def count_csv_rows(file_path):
+    count = 0
+    with open(file_path, "r", encoding= "utf-8") as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in  reader:
+            count += 1
+    return count
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    customer_count = count_csv_rows("data/customers.csv")
+    loan_count = count_csv_rows("data/loan_v3.csv")
+    repayment_count = count_csv_rows("data/repayments.csv")
+    
+    return render_template(
+        "index.html",
+        customer_count=customer_count,
+        loan_count=loan_count,
+        repayment_count=repayment_count
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
